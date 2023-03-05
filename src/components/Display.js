0175216c072218e9a6b244f3fce583375716596d
aeroneeee <pamintuan.aerone@gmail.com>
2023-03-05 14:55:21 +0800

    🚸 improve display value

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,11 +1,23 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useRef } from 'react'
 import CalculatorContext from '../contexts/CalculatorContext';
+
+import { addCommas } from '../utils'
 
 const Display = () => {
   const { displayValue } = useContext(CalculatorContext);
+
+  const displayRef = useRef(null);
+
+	useEffect(() => {
+    const display = displayRef.current;
+    const fontSize = Math.max(42 - displayValue.length, 12);
+		console.log('fontSize', fontSize)
+    display.style.fontSize = `${fontSize}px`;
+  }, [displayValue]);
+
   return (
-    <div className="display">
-      {displayValue}
+    <div className="display" ref={displayRef}>
+      {addCommas(displayValue)}
     </div>
   );
 };
